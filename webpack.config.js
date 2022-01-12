1a983e1cc9c56c164fef7c27b92dffbedff7d59e
Alek <aleksebastian@gmail.com>
2022-01-12 17:33:56 -0600

    babel and webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
+  entry: "./client/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./client/dist"),
+    filename: "photos-slider.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$/,
+    }),
+  ],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+};
