3f6edaaad47d1787cd1b2448813590e7a783fd60
Alek <aleksebastian@gmail.com>
2022-01-12 18:00:52 -0600

    Added s3 upload files

---
+++ deploy/upload-build.js
@@ -0,0 +1,25 @@
+const s3 = require("../deploy/s3.js");
+const fs = require("fs");
+const path = require("path");
+
+const uploadBuild = async (file, encoding = "") => {
+  await s3.createBucket();
+  const buildFile = fs.createReadStream(file);
+  buildFile.on("error", (err) => {
+    console.log(`File error: ${err}`);
+  });
+  const fileParts = file.split("/");
+  const fileName = fileParts[fileParts.length - 1];
+  s3.uploadToS3(`js/${fileName}`, buildFile, "text/javascript", encoding);
+};
+
+const run = async () => {
+  await uploadBuild(path.join(__dirname, "../client/dist/photos-slider.js"));
+  await uploadBuild(
+    path.join(__dirname, "../client/dist/photos-slider.js.gz"),
+    "gzip"
+  );
+  process.exit;
+};
+
+run();
